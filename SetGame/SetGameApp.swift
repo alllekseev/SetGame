6523ad36a420bed05c292aa8ce556d7c89830fcd
alllekseev <01.wftp@gmail.com>
2024-02-14 12:44:59 +0300

    Initial Commit

---
+++ SetGame/SetGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SetGameApp.swift
+//  SetGame
+//
+//  Created by Олег Алексеев on 14.02.2024.
+//
+
+import SwiftUI
+
+@main
+struct SetGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
